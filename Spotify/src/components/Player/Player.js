ce6ca3cefe09714af38dcbfabe50e25c3741dcab
JeanQ13 <jean130398@hotmail.com>
2020-12-20 12:57:01 -0700

    Mostrar canción en reproducción

--- Spotify/src/components/Player/Player.js
+++ Spotify/src/components/Player/Player.js
@@ -4,12 +4,21 @@ import { Grid, Progress, Icon, Input, Image } from "semantic-ui-react";
 
 import "./Player.scss";
 
-export default function Player() {
+export default function Player(props) {
+    //const { songData } = props;
+
+    const songData = {
+        image: "https://firebasestorage.googleapis.com/v0/b/spotify-dev-a3285.appspot.com/o/album%2Faeb2487e-a641-43bd-bc1b-3c3729e42e3b?alt=media&token=ff909b9e-ea9c-4b12-8cc8-51cd9e907343", 
+        name: "Encore"
+    };
+
     return (
+
         <div className="player">
             <Grid>
                 <Grid.Column width={4} className="left">
-                    <h2>left</h2>
+                    <Image src={songData?.image}/>
+                    {songData?.name}
                 </Grid.Column>
                 <Grid.Column width={8} className="center">
                     <h2>Center</h2>
