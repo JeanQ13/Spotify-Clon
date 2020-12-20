2f8c39ecad5d02e446dcc06c166c4aea769814ef
UrbinaArmando <urbinaa01@gmail.com>
2020-12-20 15:19:39 -0700

    Estructura del formulario

--- Spotify/src/components/Songs/AddSongForm/AddSongForm.js
+++ Spotify/src/components/Songs/AddSongForm/AddSongForm.js
@@ -1,12 +1,33 @@
 import React from 'react';
-
+import { Form, Input, Button, Icon, Dropdown } from "semantic-ui-react";
 import "./AddSongForm.scss";
 
 export default function AddSongForm(props) {
     const { setShowModal } = props;
+
+    const onSubmit = () =>{
+        console.log("enviando");
+    }
+
     return (
-        <div>
-           <h2>AddSong .....</h2> 
-        </div>
+        <Form className="add-song-form" onSubmit={onSubmit}>
+            <Form.Field>
+                <Input placeholder="Nombre de la canción"/>
+            </Form.Field>
+            <Form.Field>
+                <Dropdown
+                placeholder="Asigna la canción a un álbum"
+                search
+                selection
+                
+                />
+            </Form.Field>
+            <Form.Field>
+                <h2>UploadSong</h2>
+            </Form.Field>
+            <Button type="submit">
+                Subir canción
+            </Button>
+        </Form>
     )
 }
