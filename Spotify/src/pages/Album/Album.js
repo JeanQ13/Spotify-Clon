f8e566e838ee0d15d24dcb8fe602ed2283118fd5
mbzamoran <mbzamoran@gmail.com>
2020-12-19 23:55:17 -0700

    Renderizar informacion

--- Spotify/src/pages/Album/Album.js
+++ Spotify/src/pages/Album/Album.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Loader } from "semantic-ui-react";
 import { withRouter } from "react-router-dom";
 import firebase from "../../utils/Firebase";
 import "firebase/firestore";
@@ -46,11 +47,39 @@ function Album(props) {
         }
       }, [album]);
 
-    return (
-        <div>
-            <h2>Estamos en el album</h2>
+      if (!album || !artist) {
+        return <Loader active>Cargando...</Loader>;
+      }
+
+      return (
+        <div className="album">
+          <div className="album__header">
+            <HeaderAlbum album={album} albumImg={albumImg} artist={artist} />
+          </div>
+          <div className="album__songs">
+              <p>Lista de canciones</p>
+          </div>
         </div>
-    )
+      );
 }
 
 export default withRouter(Album);
+
+function HeaderAlbum(props) {
+    const { album, albumImg, artist } = props;
+  
+    return (
+      <>
+        <div
+          className="image"
+          style={{ backgroundImage: `url('${albumImg}')` }}
+        />
+        <div className="info">
+          <h1>{album.name}</h1>
+          <p>
+            De <span>{artist.name}</span>
+          </p>
+        </div>
+      </>
+    );
+  }
