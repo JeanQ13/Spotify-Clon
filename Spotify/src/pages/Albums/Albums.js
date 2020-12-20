86c9d4127d03bf9b7551cf2dea2a0b22d73c12ab
JeanQ13 <jean130398@hotmail.com>
2020-12-19 21:17:17 -0700

    obteniendo datos

--- Spotify/src/pages/Albums/Albums.js
+++ Spotify/src/pages/Albums/Albums.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { map } from "lodash";
+import firebase from "../../utils/Firebase";
+import "firebase/firestore";
 
 import "./Albums.scss";
 
-export default function Albums() {
+const db = firebase.firestore(firebase);
+
+export default function Albums() {  
+    const [albums, setAlbums] = useState([]);
+    console.log(albums);
+
+    useEffect(() => {
+        db.collection("albums")
+        .get()
+        .then(response =>{
+            const arrayAlbums = [];
+            map(response?.docs, album =>{
+                const data = album.data();
+                data.id = album.id;
+                arrayAlbums.push(data);
+            });
+            setAlbums(arrayAlbums);
+        })
+    }, [])
+    
     return (
-        <div>
-            <h2>Albums</h2>
+        <div className="albums">
+            <h1>Albumes</h1>
         </div>
     )
 }
