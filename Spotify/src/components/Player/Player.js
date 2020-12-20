5b3df716fee59e51c70a77e023de996e3a6fa563
JeanQ13 <jean130398@hotmail.com>
2020-12-20 12:46:55 -0700

    Estructura del reproductor

--- Spotify/src/components/Player/Player.js
+++ Spotify/src/components/Player/Player.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import ReactPlayer from "react-player";
+import { Grid, Progress, Icon, Input, Image } from "semantic-ui-react";
 
 import "./Player.scss";
 
 export default function Player() {
     return (
-        <div>
-            <h1>Player----</h1>
+        <div className="player">
+            <Grid>
+                <Grid.Column width={4} className="left">
+                    <h2>left</h2>
+                </Grid.Column>
+                <Grid.Column width={8} className="center">
+                    <h2>Center</h2>
+                </Grid.Column>
+                <Grid.Column width={4} className="right">
+                    <h2>right</h2>
+                </Grid.Column>
+            </Grid>
         </div>
     )
 }
