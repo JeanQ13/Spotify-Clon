f16793b2b79f1c17cae1d024e66c7cd8ca182bdd
UrbinaArmando <urbinaa01@gmail.com>
2020-12-19 23:12:02 -0700

    pagina album y rutas

---
+++ Spotify/src/pages/Album/Album.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { withRouter } from "react-router-dom";
+
+import "./Album.scss";
+
+function Album(porps) {
+    
+    return (
+        <div>
+            <h2>Estamos en el album</h2>
+        </div>
+    )
+}
+
+export default withRouter(Album);
