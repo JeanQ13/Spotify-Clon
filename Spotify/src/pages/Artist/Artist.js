dbf052316cf10d2bb2773a70bea4c7f5dc004326
mbzamoran <mbzamoran@gmail.com>
2020-12-20 00:01:31 -0700

    Obtener albunes del perfil artista

--- Spotify/src/pages/Artist/Artist.js
+++ Spotify/src/pages/Artist/Artist.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from "react-router-dom";
 import BannerArtist from "../../components/Artists/BannerArtist";
+import { map } from "lodash";
 import firebase from "../../utils/Firebase";
 import "firebase/firebase";
 
@@ -12,15 +13,35 @@ const db = firebase.firestore(firebase);
 function Artist(props) {
     const { match } = props;
     const [artist, setArtist] = useState(null);
+    const [albums, setAlbums] = useState(null);
 
     useEffect(() => {
         db.collection("artists")
         .doc(match?.params?.id)
         .get()
         .then(response =>{
-            setArtist(response.data());
+            const data = response.data();
+            data.id = response.id;
+            setArtist(data);
         });
     }, [match])
+
+    useEffect(() => {
+        if (artist) {
+          db.collection("albums")
+            .where("artist", "==", artist.id)
+            .get()
+            .then(response => {
+              const arrayAlbums = [];
+              map(response?.docs, album => {
+                const data = album.data();
+                data.id = album.id;
+                arrayAlbums.push(data);
+              });
+              setAlbums(arrayAlbums);
+            });
+        }
+      }, [artist]);
 
     return (
         <div className="artist">
