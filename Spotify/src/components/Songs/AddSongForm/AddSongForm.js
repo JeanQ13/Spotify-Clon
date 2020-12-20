fbb05b63b55023fb313b5be0b47480588237a0e6
UrbinaArmando <urbinaa01@gmail.com>
2020-12-20 15:34:35 -0700

    Obtener albumes

--- Spotify/src/components/Songs/AddSongForm/AddSongForm.js
+++ Spotify/src/components/Songs/AddSongForm/AddSongForm.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Input, Button, Icon, Dropdown } from "semantic-ui-react";
+import { map } from "lodash";
+import firebase from "../../../utils/Firebase";
+import "firebase/firestore";
 import "./AddSongForm.scss";
+
+
+const db = firebase.firestore(firebase);
 
 export default function AddSongForm(props) {
     const { setShowModal } = props;
+    const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    db.collection("albums")
+      .get()
+      .then(response => {
+        const albumsArray = [];
+        map(response?.docs, album => {
+            console.log(response);
+          const data = album.data();
+          albumsArray.push({
+            key: album.id,
+            value: album.id,
+            text: data.name
+          });
+        });
+        setAlbums(albumsArray);
+      });
+  }, []);
 
     const onSubmit = () =>{
         console.log("enviando");
@@ -19,7 +44,7 @@ export default function AddSongForm(props) {
                 placeholder="Asigna la canción a un álbum"
                 search
                 selection
-                
+                options={albums}
                 />
             </Form.Field>
             <Form.Field>
