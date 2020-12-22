364de1b2ac3e8acb955e8e2e7dd0bfb1fa209cde
JeanQ13 <jean130398@hotmail.com>
2020-12-21 21:03:04 -0700

    Estructura lista de canciones

--- Spotify/src/components/Songs/ListSongs/ListSongs.js
+++ Spotify/src/components/Songs/ListSongs/ListSongs.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Table, Icon } from "semantic-ui-react";
+import { map } from "lodash";
 
 import "./ListSongs.scss";
 
@@ -7,8 +9,44 @@ export default function ListSongs(props) {
 
 
     return (
-        <div>
-            
-        </div>
+        <Table inverted className="list-songs">
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell/>
+                    <Table.HeaderCell>
+                        Título
+                    </Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
+                <Table.Row>
+                    <Table.Cell collapsing>
+                        <Icon name="play circle outline" >
+                        </Icon>
+                    </Table.Cell>
+                    <Table.Cell>
+                        Canción 01
+                    </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.Cell collapsing>
+                        <Icon name="play circle outline" >
+                        </Icon>
+                    </Table.Cell>
+                    <Table.Cell>
+                        Canción 03
+                    </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                    <Table.Cell collapsing>
+                        <Icon name="play circle outline" >
+                        </Icon>
+                    </Table.Cell>
+                    <Table.Cell>
+                        Canción 03
+                    </Table.Cell>
+                </Table.Row>
+            </Table.Body>
+        </Table>
     )
 }
